86fd1c715e43ca05be1b4e5386571651c64ff073
Lukáš Tomáš Petrželka <petrzluk@cvut.cz>
2024-11-02 18:10:50 +0100

    Upload random model

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -8,10 +8,10 @@ for f in [PLAYERS_FILE, GAMES_FILE]:
     if not os.path.exists(f):
         raise FileNotFoundError(f)
 
-games = pd.read_csv(GAMES_FILE)
+# games = pd.read_csv(GAMES_FILE)
 
 INPUT_MATCH_COUNT = 5                       # Number of input matches for neural network
-MATCH_DATAPOINT_COUNT = games.shape[1] - 1  # Number of datapoints per match; -1 to exclude 'Unnamed: 0' column
+MATCH_DATAPOINT_COUNT = 41 #games.shape[1] - 1  # Number of datapoints per match; -1 to exclude 'Unnamed: 0' column
 
 INPUT_TEAM_COUNT = INPUT_MATCH_COUNT * MATCH_DATAPOINT_COUNT
 NN_INPUT_SIZE = INPUT_TEAM_COUNT * 2
@@ -21,4 +21,12 @@ STACK_SIZE = 50
 # hyperparameters for evaluation
 GENERATIONS = 50
 POPULATION_SIZE = 20
-MUTATION_PROB = 0.1
+assert POPULATION_SIZE % 2 == 0, "Population size must be odd"
+
+SELECTION_RATIO = 0.5
+SELECTION_SIZE = int(POPULATION_SIZE * SELECTION_RATIO)
+assert SELECTION_SIZE % 2 == 0, "Selection size must be even"
+
+CROSSOVER_PROB = 0.9
+MUTATION_PROB = 0.2
+

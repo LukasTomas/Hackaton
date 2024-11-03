de81df434655640cc0090c4896a8481943859a88
Filip Kolomazník <filipkolomaznik@proton.me>
2024-11-03 12:04:34 +0100

    Speed up (Date--)

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -1,18 +1,17 @@
 import torch
 import pandas as pd
 import numpy as np
-
+import time
 class DataPreprocessing:
     def GetTensor(self, team_A_data, team_H_data):
         A_data = self.Processing(team_A_data)
         H_data = self.Processing(team_H_data)
-
         Tensor = self.Tenzor(A_data, H_data)
         return Tensor                                                               # return torch.Tensor
 
     def Processing(self, data):
         data = pd.DataFrame(data)
-        data = self.Change_Date(data)
+        data = self.Destroy_Date(data)
         data = self.Destroy_Open(data)
         return data
 
@@ -23,16 +22,8 @@ class DataPreprocessing:
         data = data.to_numpy().flatten().astype(np.float32)
         return torch.tensor(data)
 
-    def Change_Date(self, data_to_transform):                                       # Changing Date to int (in the process year is lost)
+    def Destroy_Date(self, data_to_transform):
         if "Date" in data_to_transform.columns:
-            data_to_transform["Day"] = pd.NA
-            data_to_transform["Month"] = pd.NA
-            for index ,row in data_to_transform.iterrows():
-                Date = row["Date"]
-                Digits = ((str)(Date)).split("-")
-                LastDigits = Digits[2].split(" ")
-                data_to_transform.loc[index, ["Day"]] = int(LastDigits[0])
-                data_to_transform.loc[index, ["Month"]] = int(Digits[1])
             data_to_transform.drop(columns=["Date"], inplace=True)
         return data_to_transform
 
